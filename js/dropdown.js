8e14654413de5e57604a77619669d8c2755cdc3e
Matias Massaccesi <matiasmassaccesi@hotmail.com>
2024-08-02 17:37:49 -0300

    Add custom dropdowns

---
+++ js/dropdown.js
@@ -0,0 +1,35 @@
+const onClickOutside = (element, callback) => {
+  document.addEventListener('click', (e) => {
+    if (!element.contains(e.target)) callback();
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.dropdown').forEach((dropdown) => {
+    const button = dropdown.querySelector('.dropdown-trigger');
+    const selected = dropdown.querySelector('.dropdown-trigger span');
+    const input = dropdown.querySelector('input');
+    const list = dropdown.querySelector('.dropdown-list');
+    const options = list.querySelectorAll('button');
+
+    const close = () => dropdown.classList.remove('open');
+
+    onClickOutside(dropdown, close);
+
+    button.addEventListener('click', () => {
+      dropdown.classList.toggle('open');
+    });
+
+    options.forEach((option) => {
+      option.addEventListener('click', () => {
+        const text = option.querySelector('span').innerHTML;
+        selected.innerHTML = text;
+        close();
+        options.forEach((o) => o.classList.remove('active'));
+        option.classList.add('active');
+        input.value = option.dataset.value;
+        input.dispatchEvent(new Event('change'));
+      });
+    });
+  });
+});
